30dbbce758aa7573c4b86696a89505e5dd4df210
xuquezi <1377262954@qq.com>
2018-10-26 00:32:58 +0800

    Site updated: 2018-10-26 00:32:44

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "莉莉安",
+        artist: '宋冬野',
+        url: 'http://www.ytmp3.cn/down/34105.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/17.jpg',
+      }
+    ]
+});
