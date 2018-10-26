f846b09ea70a2f61b532e5ce6342f2784c3eba06
xuquezi <1377262954@qq.com>
2018-10-26 09:45:31 +0800

    Site updated: 2018-10-26 09:45:14

--- dist/music.js
+++ dist/music.js
@@ -6,8 +6,8 @@ const ap = new APlayer({
       {
         name: "春风十里",
         artist: '鹿先森',
-        url: 'http://www.ytmp3.cn/down/39154.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/17.jpg',
+        url: '/resources/music/chunfengshili.mp3',
+        cover: '/resources/images/luxiansen.jpg',
       }
     ]
 });
