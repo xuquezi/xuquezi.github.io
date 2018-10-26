d88198dd57b5483c8cce4cef3b523f4383389c21
xuquezi <1377262954@qq.com>
2018-10-26 09:25:03 +0800

    Site updated: 2018-10-26 09:24:56

--- dist/music.js
+++ dist/music.js
@@ -4,9 +4,9 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "莉莉安",
-        artist: '宋冬野',
-        url: 'http://www.ytmp3.cn/down/34105.mp3',
+        name: "春风十里",
+        artist: '鹿先森',
+        url: 'http://www.ytmp3.cn/down/39154.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/17.jpg',
       }
     ]
