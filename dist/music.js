b6cb38dfd1b6f70a5cbb2e2a2e7ec742c22a962d
xuquezi <1377262954@qq.com>
2019-04-13 10:14:18 +0800

    Site updated: 2019-04-13 10:14:10

--- dist/music.js
+++ dist/music.js
@@ -6,21 +6,8 @@ const ap = new APlayer({
       {
         name: "春风十里",
         artist: '鹿先森',
-        url: '/music/chunfengshili.mp3',
-        cover: '/images/luxiansen.jpg',
-      },
-	  {
-        name: "很久以前",
-        artist: '鹿先森',
-        url: '/music/henjiuyiqian.mp3',
-        cover: '/images/luxiansen.jpg',
-      },
-	  {
-        name: "失眠",
-        artist: '鹿先森',
-        url: '/music/shimian.mp3',
-        cover: '/images/luxiansen.jpg',
+        url: '/music/xiangyu.mp3',
+        cover: '/images/dayu.jpeg',
       }
-	  
     ]
 });
